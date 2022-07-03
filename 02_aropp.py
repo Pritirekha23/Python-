8051fe6e67566beb48668ce16bb70dc00fd27a06
unknown <iampritirekha@gmail.com>
2022-07-03 00:49:34 -0700

    my first commit

---
+++ 02_aropp.py
@@ -0,0 +1,5 @@
+#adding of two numbers
+a=10
+b=5
+c=a+b
+print(c)
---
+++ 03_var.py
@@ -0,0 +1,8 @@
+# Variable--It is just like a container which stores value 
+#or Variable means memory location
+#it is changable,it is the combination of vari+able
+#here a is the variable name and 156 is the value 
+a=156
+print(a)
+print(a/2)
+print(a*3+2-4)
