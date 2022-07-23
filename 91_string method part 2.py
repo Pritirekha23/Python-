d2f5ab420d11a874aeaf1261f99bd97e3788f2f2
unknown <iampritirekha@gmail.com>
2022-07-22 22:48:06 -0700

    programs

---
+++ 91_string method part 2.py
@@ -0,0 +1,25 @@
+# string method part 2
+#splitting of string 
+#split()  ----   it will used to return a list ,once it found spcae means it treated as one element
+print('split method ex1')
+s='pritirekha panda'
+print(s.split())
+
+print('split method ex1')
+s='pritirekha panda'
+print(s.split('a'))
+
+
+print('split method ex3')
+s='pritirekha panda'
+print(s.split('p'))
+
+print('spli method ex4')
+dob='1-1-2000'
+print(dob.split('-'))
+#join()
+#istrip()
+#strip()
+#rjust()
+#ijust()
+#
